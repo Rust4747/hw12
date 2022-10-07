ac03543030f282f548eba17b36b7cd57a0f580fb
Tony <yaraliev5@gmail.com>
2022-10-07 23:56:37 +0300

    hw12

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,13 @@ public class Main {
     public static void main(String[] args) {
         Author arturcheyli=new Author("Артур", "Хейли");
         Author fyodordostoevsky=new Author("Федор", "Достоевский");
+        if (arturcheyli.equals(arturcheyli)) {
+            System.out.println("объкты равны");
+        }
+        if (arturcheyli.hashCode() == fyodordostoevsky.hashCode()) {
+            System.out.println("хэшкоды равны");
+        }
+
         Book theFinalDiagnosis=new Book ("Окончательный диагноз ", arturcheyli,1959);
         Book theIdiot=new Book("Идиот", fyodordostoevsky, 1868);
         System.out.println("Книга:"+theFinalDiagnosis.getTitle()+ " Автор:"+theFinalDiagnosis.getAuthor().getName()
@@ -11,6 +18,7 @@ public class Main {
         theFinalDiagnosis.setPublicationYear(2005);
         System.out.println("Книга:"+theFinalDiagnosis.getTitle()+ " Автор:"+theFinalDiagnosis.getAuthor().getName()
                 + " "+theFinalDiagnosis.getAuthor().getSurname()+ " Год публикации:" + theFinalDiagnosis.getPublicationYear());
+        System.out.println(theIdiot);
 
 
     }
